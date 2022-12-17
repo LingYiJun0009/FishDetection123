48ecf592a3ec5e68cbbd0950f3040982bf864343
seven <taiwanlingyj80@gmail.com>
2022-12-17 17:05:30 +0800

    after a while 17th Dec 2022

--- cluster_fusion/manager_2.py
+++ cluster_fusion/manager_2.py
@@ -17,6 +17,7 @@ import cv2
 from natsort import natsorted
 # what if I change something
 # get the latest written test run folder
+# test if it did get pushed
 pathtorunsdata_test = "../Data/Runs/Test/"
 latesttest = natsorted(os.listdir(pathtorunsdata_test))[-1]
 toggle = 2
